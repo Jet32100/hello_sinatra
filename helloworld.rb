34519445a30dc75e1410b2095efbba45bf9585af
Jamie Pilgrim <hello@jamiepilgrim.com>
2016-02-04 11:28:55 -0600

    very basic mvc sinatra structure

---
+++ helloworld.rb
@@ -0,0 +1,23 @@
+### STEP ONE #######################################	
+
+	### IN TERMINAL, ENTER: gem install sinatra ####
+
+	#require sinatra
+
+
+	#write your first get route
+
+	### IN TERMINAL, ENTER: ruby name_of_file.rb ####
+
+
+####################################################
+
+### STEP TWO ######################################
+
+	#write a second route that takes in parameters through the route
+		# return that parameter in the view
+
+
+####################################################
+
+### STEP THREE ######################################
